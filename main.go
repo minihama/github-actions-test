a4714851bdd3b86bc619634c388c0bc0778493c4
sample01 <minihama@hotmail.co.kr>
2025-08-06 16:38:33 +0900

    change main.go

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func main() {
 	http.HandleFunc("/", helloHandler)
 	fmt.Println("Server starting on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("Error starting server: %s
-", err)
+		fmt.Printf("Error starting server: %s\n", err)
 	}
 }
